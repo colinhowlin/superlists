497ecf9d10b44b6a9251999c1f62f598cee3c3dc
colinhowlin <colin@bcuz.eu>
2019-06-18 15:16:51 +0100

    Modified functional tests to check for multiple list items

--- functional_tests.py
+++ functional_tests.py
@@ -41,12 +41,20 @@ class NewVisitorTest(unittest.TestCase):
         
         #Text box is still present on the page 
         #Type "Use peacock feathers to make a fly"
-        self.fail('finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
         
         #Page updates again and should now show both items
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1. Buy peacock feathers', [row.text for row in rows])        
+        self.assertIn('2. Use peacock feathers to make a fly', [row.text for row in rows])
 
         #Site has generated a unique URL for the list
         #Should be explanatory text for this.
+        self.fail('finish the test!')
 
         #When the URL is visited, the saved itesm are there.
 
