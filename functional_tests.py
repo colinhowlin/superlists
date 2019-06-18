8d8024b2192e1b2716750925a89d1b9ad44bbc73
colinhowlin <colin@bcuz.eu>
2019-06-18 15:23:20 +0100

    Refactored functional test to remove duplicate code

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,11 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):
          self.browser.quit()
+         
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         #Open the homepage of the app
@@ -34,10 +39,7 @@ class NewVisitorTest(unittest.TestCase):
         #"1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1. Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1. Buy peacock feathers')
         
         #Text box is still present on the page 
         #Type "Use peacock feathers to make a fly"
@@ -47,10 +49,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
         
         #Page updates again and should now show both items
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1. Buy peacock feathers', [row.text for row in rows])        
-        self.assertIn('2. Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1. Buy peacock feathers')
+        self.check_for_row_in_list_table('2. Use peacock feathers to make a fly')
 
         #Site has generated a unique URL for the list
         #Should be explanatory text for this.
