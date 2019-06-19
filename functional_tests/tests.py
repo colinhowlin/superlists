4bf53efdcece968a4d886631ff9dcac230990a66
colinhowlin <colin@bcuz.eu>
2019-06-19 20:52:10 +0100

    Migrate to LiveServerTestCase, remove explicit waits from FT

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,13 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+MAX_WAIT = 10
+
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -11,14 +15,23 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
          self.browser.quit()
          
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
-
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+                
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+        
     def test_can_start_a_list_and_retrieve_it_later(self):
         #Open the homepage of the app
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         #Page title and header should mention to-do lists
         self.assertIn("To-Do", self.browser.title)
@@ -38,25 +51,20 @@ class NewVisitorTest(unittest.TestCase):
         #Hit enter - page updates and the page now lists
         #"1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1. Buy peacock feathers')
+        self.wait_for_row_in_list_table('1. Buy peacock feathers')
         
         #Text box is still present on the page 
         #Type "Use peacock feathers to make a fly"
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
         
         #Page updates again and should now show both items
-        self.check_for_row_in_list_table('1. Buy peacock feathers')
-        self.check_for_row_in_list_table('2. Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1. Buy peacock feathers')
+        self.wait_for_row_in_list_table('2. Use peacock feathers to make a fly')
 
         #Site has generated a unique URL for the list
         #Should be explanatory text for this.
         self.fail('finish the test!')
 
-        #When the URL is visited, the saved itesm are there.
-
-if __name__ == '__main__':
-    unittest.main()
+        #When the URL is visited, the saved items are there.
